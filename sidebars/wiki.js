1331177ca23dac67cd2a20f3338e4fa411d56087
kaicardenas0618 <kcardenasit@gmail.com>
2026-02-24 14:10:36 -0500

    Map Manager Optional Features

--- sidebars/wiki.js
+++ sidebars/wiki.js
@@ -4,7 +4,7 @@ module.exports = {
     {
       type: 'category',
       label: 'Maps',
-      items: ['maps/map-directory', 'maps/map-installation-guide', 'maps/legacy-map-installation-guide', 'maps/making-custom-maps'],
+      items: ['maps/map-directory', 'maps/map-installation-guide', 'maps/legacy-map-installation-guide', 'maps/making-custom-maps', 'maps/optional-features'],
     },
     {
       type: 'category',
