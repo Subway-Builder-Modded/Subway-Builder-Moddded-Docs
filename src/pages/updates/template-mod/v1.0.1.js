19d549b827fadbb74bebe7848c841be41f267506
kaicardenas0618 <kcardenasit@gmail.com>
2026-02-26 00:14:03 -0500

    Update Release Notes

--- src/pages/updates/template-mod/v1.0.1.js
+++ src/pages/updates/template-mod/v1.0.1.js
@@ -13,9 +13,11 @@ export default function TemplateMod_UpdateV1_0_1() {
         features: [],
         bugfixes: [],
         upgrades: [
-            { id: "updates.template-mod.v1-0-1.upgrades1", message: "`electron.d.ts` => There's a few more useful methods for modders that aren't currently listed" },
-            { id: "updates.template-mod.v1-0-1.upgrades2", message: "`game-state.d.ts` => Some of the schema definitions do not match what the game returns at runtime (namely Commute schemas)" },
-            { id: "updates.template-mod.v1-0-1.upgrades3", message: "`Added a useful getLanguage API and clarifying that country is optional for some cities" },
+            { id: "updates.template-mod.v1-0-1.upgrades1", message: "electron.d.ts => There's a few more useful methods for modders that aren't currently listed" },
+            { id: "updates.template-mod.v1-0-1.upgrades2", message: "game-state.d.ts => Some of the schema definitions do not match what the game returns at runtime (namely Commute schemas)" },
+            { id: "updates.template-mod.v1-0-1.upgrades3", message: "Added a useful getLanguage API and clarifying that country is optional for some cities" },
+            { id: "updates.template-mod.v1-0-1.upgrades4", message: "Add additional optional fields to ModManifest (these are not clearly documented but are confirmed to be parsed by the game at runtime)" },
+            { id: "updates.template-mod.v1-0-1.upgrades5", message: "Add additional exposed electron methods" },
         ],
         otherNotes: [],
       }}
