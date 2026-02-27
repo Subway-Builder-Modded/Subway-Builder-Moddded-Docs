219b5768f0a1886f23a607594ae9ba6a8a46da61
kaicardenas0618 <kcardenasit@gmail.com>
2026-02-26 23:37:46 -0500

    404 Train Function

--- src/theme/NotFound/index.js
+++ src/theme/NotFound/index.js
@@ -3,6 +3,42 @@ import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import styles from "./styles.module.css";
 
+const SVGComponent = (props) => (
+  <svg viewBox="0 0 500 500" className={styles.train} {...props}>
+    <g stroke="currentColor" fill="none" strokeWidth={2}>
+      <circle cx={226} cy={19} r={19} />
+      <circle cx={275} cy={19} r={19} />
+      <rect x={100} y={44} rx={65} ry={65} width={300} height={370} />
+      <rect
+        x={207}
+        y={58}
+        rx={11}
+        ry={11}
+        width={87}
+        height={41}
+        fill="currentColor"
+      />
+      <rect
+        x={136}
+        y={112}
+        rx={34}
+        ry={34}
+        width={229}
+        height={111}
+        fill="currentColor"
+      />
+      <circle cx={166} cy={349} r={30} fill="currentColor" />
+      <circle cx={334} cy={349} r={30} fill="currentColor" />
+    </g>
+    <polyline
+      points="96,530 162,432 338,432 404,530"
+      stroke="currentColor"
+      strokeWidth={36}
+      fill="none"
+    />
+  </svg>
+);
+
 function NumberBullet({ value, color, hoverColor }) {
   return (
     <span
@@ -21,25 +57,8 @@ export default function NotFound() {
   return (
     <Layout title="404 - Page Not Found">
       <main className={styles.wrapper}>
-        {/* Background Train */}
         <div className={styles.trainWrapper}>
-          <svg viewBox="0 0 500 500" className={styles.train}>
-            <g stroke="currentColor" fill="none" strokeWidth="2">
-              <circle cx="226" cy="19" r="19" />
-              <circle cx="275" cy="19" r="19" />
-              <rect x="100" y="44" rx="65" ry="65" width="300" height="370" />
-              <rect x="207" y="58" rx="11" ry="11" width="87" height="41" fill="currentColor" />
-              <rect x="136" y="112" rx="34" ry="34" width="229" height="111" fill="currentColor" />
-              <circle cx="166" cy="349" r="30" fill="currentColor" />
-              <circle cx="334" cy="349" r="30" fill="currentColor" />
-            </g>
-            <polyline
-              points="96,530 162,432 338,432 404,530"
-              stroke="currentColor"
-              strokeWidth="36"
-              fill="none"
-            />
-          </svg>
+          <SVGComponent />
         </div>
 
         <div className={styles.content}>
